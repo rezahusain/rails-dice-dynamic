d354e5bfd3ef86c7fdcebde896fafd1882d4b9a9
Reza Husain <84816920+rezahusain@users.noreply.github.com>
2024-06-03 15:13:24 +0000

    Finished

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,17 @@
+class DiceController < ApplicationController
+  def home
+    render(:template=>"pages_templates/home")
+  end
+  def result
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+
+      @rolls.push(die)
+    end
+    render(:template=>"pages_templates/result")
+  end
+end
